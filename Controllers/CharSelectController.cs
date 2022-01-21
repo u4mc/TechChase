d41fb0833ff12de370a8539b6e770284944809e7
u4mc <mileschampion@hotmail.co.uk>
2022-01-21 15:41:06 +0000

    n

---
+++ Controllers/CharSelectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechChase.Controllers
+{
+    public class CharSelectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,8 @@
+﻿namespace TechChase.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public Uri Image { get; set; }
+    }
+}
